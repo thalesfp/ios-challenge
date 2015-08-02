f5b581b713ff3a7c5987a9aa2afeab274815708b
Thales Pereira <thalesfp@gmail.com>
2015-08-02 01:19:33 -0300

    Initial Commit

---
+++ ios-challenge/ios-challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-challenge
+//
+//  Created by Thales Pereira on 8/2/15.
+//  Copyright (c) 2015 Thales Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
